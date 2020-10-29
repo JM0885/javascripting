f52e111249c533d7712bc16798c2449f59dfd2b8
Jared Mosher <jared.mosher@gmail.com>
2020-10-29 16:03:47 +0000

    Initial commit with all my work so far

---
+++ javascripting/looping-through-arrays.js
@@ -0,0 +1,16 @@
+let pets = ['cat', 'dog', 'rat']
+
+for (let i=0; i < pets.length; i++) {
+  pets[i] = pets[i] + 's';
+}
+console.log(pets)
+
+/*let total = 0
+    const limit = 10
+    
+    for (let i = 0; i < limit; i++) {
+      total += i
+    }
+    
+    console.log(total)*/
+
